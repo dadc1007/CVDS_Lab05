5de1f319863dbd6e4b15902b165b6e5bda114bea
dadc1007 <dadica1007@gmail.com>
2025-02-23 00:42:30 -0500

    Implemente el endpoint para crear el usuario

--- src/main/java/edu/eci/UniReserva/UniReserva_Backend/controller/UserController.java
+++ src/main/java/edu/eci/UniReserva/UniReserva_Backend/controller/UserController.java
@@ -1,15 +1,29 @@
 package edu.eci.UniReserva.UniReserva_Backend.controller;
 
+import edu.eci.UniReserva.UniReserva_Backend.model.User;
 import edu.eci.UniReserva.UniReserva_Backend.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/user")
 public class UserController {
     private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        try {
+            String response = userService.createUser(user);
+            return ResponseEntity.ok(response);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
