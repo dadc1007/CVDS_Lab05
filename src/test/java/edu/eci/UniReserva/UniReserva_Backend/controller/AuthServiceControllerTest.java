5f5cb310db7e3341e47da9b6d6ac54d42d563ca5
ChenteChaurio <vicentegarzonrios@gmail.com>
2025-03-02 20:55:46 -0500

    Solucionando problemas de dependencias y test

--- src/test/java/edu/eci/UniReserva/UniReserva_Backend/controller/AuthServiceControllerTest.java
+++ src/test/java/edu/eci/UniReserva/UniReserva_Backend/controller/AuthServiceControllerTest.java
@@ -7,8 +7,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -27,7 +27,7 @@ public class AuthServiceControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
+    @MockitoBean
     private AuthServiceImpl authServiceImpl;
 
     @Test
