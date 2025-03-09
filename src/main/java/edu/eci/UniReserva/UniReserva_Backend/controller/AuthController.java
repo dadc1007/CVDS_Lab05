a0e67e3faf8fcf3aed8451972416d692c3eae296
dadc1007 <dadica1007@gmail.com>
2025-03-08 22:00:35 -0500

    En la peticion de crear usuario, ya no devuelve un mensaje sino el objeto

--- src/main/java/edu/eci/UniReserva/UniReserva_Backend/controller/AuthController.java
+++ src/main/java/edu/eci/UniReserva/UniReserva_Backend/controller/AuthController.java
@@ -2,14 +2,18 @@ package edu.eci.UniReserva.UniReserva_Backend.controller;
 
 
 import edu.eci.UniReserva.UniReserva_Backend.model.LoginRequest;
+import edu.eci.UniReserva.UniReserva_Backend.model.User;
 import edu.eci.UniReserva.UniReserva_Backend.service.AuthService;
 import edu.eci.UniReserva.UniReserva_Backend.service.impl.AuthServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/auth")
@@ -27,6 +31,16 @@ public class AuthController {
             return ResponseEntity.ok(message);
         }catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<Object> createUser(@RequestBody User user) {
+        try {
+            User createdUser = authService.authenticateSignUp(user);
+            return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
         }
     }
 }
